895bc2e29f9a1b4952323279f73f89ba2a76a893
PQCraft <58464017+PQCraft@users.noreply.github.com>
2021-05-07 07:07:48 -0400

    Add files via upload

---
+++ termutil.h
@@ -0,0 +1,136 @@
+/* TermUtil *nix terminal utility library 1.0 (C) 2021 PQCraft */
+
+#ifndef TERMUTIL_H
+#define TERMUTIL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+struct termios term, restore;
+static struct termios orig_termios;
+
+bool textlock = false;
+
+int curx, cury;
+
+struct cursor {
+    int x;
+    int y;
+};
+
+struct cursor* srstack = NULL;
+int srstackp = 0;
+
+int kbhit() {
+    static const int STDIN = 0;
+    static bool initialized = false;
+    if (!initialized) {
+        struct termios term;
+        tcgetattr(STDIN, &term);
+        term.c_lflag &= ~ICANON;
+        tcsetattr(STDIN, TCSANOW, &term);
+        setbuf(stdin, NULL);
+        initialized = true;
+    }
+    int bytes;
+    ioctl(STDIN, FIONREAD, &bytes);
+    return bytes;
+}
+
+struct cursor getCurPos() {
+    fflush(stdout);
+    char buf[16];
+    register int ret, i;
+    i = kbhit();
+    while (i > 0) {getchar(); i--;}
+    if (!textlock) {
+        tcgetattr(0, &term);
+        tcgetattr(0, &restore);
+        term.c_lflag &= ~(ICANON|ECHO);
+        tcsetattr(0, TCSANOW, &term);
+    }
+    fputs("\e[6n", stdout);
+    fflush(stdout);
+    ret = read(1, &buf, 16);
+	struct cursor cur = {0, 0};
+    if (!ret) {
+        if (!textlock) tcsetattr(0, TCSANOW, &restore);
+        return cur;
+    }
+	if (!textlock) tcsetattr(0, TCSANOW, &restore);
+    sscanf(buf, "\e[%d;%dR", &cur.y, &cur.x);
+    return cur;
+}
+
+void saveCurPos() {
+    srstackp++;
+    srstack = (struct cursor*)realloc(srstack, srstackp * sizeof(srstack));
+    srstack[srstackp - 1] = getCurPos();
+}
+
+void restoreCurPos() {
+    if (srstackp < 0) return;
+    srstackp--;
+    printf("\e[%d;%dH", srstack[srstackp].y, srstack[srstackp].x);
+    fflush(stdout);
+    if (srstackp) srstack = (struct cursor*)realloc(srstack, srstackp * sizeof(srstack));
+}
+
+void enableRawMode() {
+    if (!textlock) {
+        tcgetattr(0, &term);
+        tcgetattr(0, &restore);
+        term.c_lflag &= ~(ICANON|ECHO);
+        tcsetattr(0, TCSANOW, &term);
+    }
+}
+
+void disableRawMode() {
+    if (textlock) {
+        tcsetattr(0, TCSANOW, &restore);
+    }
+}
+
+bool tty() {
+    return (bool)!system("tty -s 1> /dev/null 2> /dev/null");
+}
+
+double randDouble(double min, double max) {
+    double range = max - min;
+    double div = RAND_MAX / range;
+    return min + (rand() / div);
+}
+
+void upCase(char* str) {
+    for (int i = 0; str[i]; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') str[i] -= 32;
+    }
+}
+
+void lowCase(char* str) {
+    for (int i = 0; str[i]; i++) {
+        if (str[i] >= 'A' && str[i] <= 'Z') str[i] += 32;
+    }
+}
+
+void strcatchar(char* str, char c) {
+    int len = 0;
+    while (str[len]) {len++;}
+    str[len] = c;
+    len++;
+    str[len] = 0;
+}
+
+void strcpysnip(char* str2, char* str1, int i, int j) {
+    if (!str1) {str2[0] = 0; return;}
+    int i2 = 0;
+    for (int i3 = i; i3 < j; i3++) {str2[i2] = str1[i3]; i2++;}
+    str2[i2] = 0;
+}
+
+#endif
